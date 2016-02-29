b9b2f9a175ae8ab9bde1cf81b69b2a27886c853b
Vedant Pathak <Vedant@Vedants-MacBook-Pro.local>
2016-02-29 14:26:02 -0600

    changed the file

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,55 @@
 # Code your instances here
+class GardenGnome
+
+	def name= (name)
+		@name = name
+	end
+
+	def name
+		return @name
+	end
+
+	def age= (age)
+		@age = age
+	end
+
+	def age
+		return @age
+	end
+
+	def gluten_allergy= (gluten_allergy)
+		@gluten_allergy = gluten_allergy
+	end
+
+	def gluten_allergy
+		return @gluten_allergy
+	end
+
+	def initialize(personality = "evil", hat_color = "red")
+		@personality = personality
+		@hat_color = hat_color
+	end
+
+	def personality
+		return @personality
+	end
+
+	def hat_color
+		return @hat_color
+	end
+
+	def gnaw
+		return "Gnawing on a tree!!!"
+	end
+
+	def shout
+		return "GNARLY!!!"
+	end
+
+	def introduce_self
+		return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+	end
+end
+#test_gnome1 = GardenGnome.new
+#test_gnome1.name ("Carl the Crappy")
+#puts test_gnome1.name
